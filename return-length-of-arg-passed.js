a37b4acd9e3076428eecd2d57eed20a7355f50b2
mitu1403 <mmitu163069@bscse.uiu.ac.bd>
2023-09-18 19:31:35 +0600

    leetCode-2703

---
+++ return-length-of-arg-passed.js
@@ -0,0 +1,18 @@
+/*
+Write a function argumentsLength that returns the count of arguments passed to it.
+ 
+
+Example 1:
+
+Input: argsArr = [5]
+Output: 1
+Explanation:
+argumentsLength(5); // 1
+
+One value was passed to the function so it should return 1.
+*/
+
+var argumentsLength = function(...args) {
+  return args.length
+ 
+};
